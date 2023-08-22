265ad49eb24044c0503d9db481282ea35297a14d
hugo.cruz <hugo.cruz@sivasdescalzo.com>
2023-08-22 11:34:39 +0200

    Feature: Add Clean If Statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CleanIfStatements from "./Clean-If-Statements";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project with the Info Graphics by George Moller! 🚀</h1>
+      <h2>Your twitter: @_georgemoller</h2>
+      <main>
+        <CleanIfStatements />
+      </main>
     </div>
   );
 }
---
+++ src/Clean-If-Statements/index.tsx
@@ -0,0 +1,65 @@
+export default function CleanIfStatements() {
+  const canUserPlaceOrderOriginal = (payment: any, product: any) => {
+    let canPlaceOrder = false;
+    if (product.hasStock === true) {
+      if (payment.processed === true) {
+        if (payment.canPay === true) {
+          canPlaceOrder = true;
+        } else {
+          canPlaceOrder = false;
+        }
+      } else {
+        canPlaceOrder = false;
+      }
+    }
+    return canPlaceOrder;
+  };
+
+  const canUserPlaceOrderRefactorOne = (payment: any, product: any) => {
+    if (product.hasStock === true) {
+      if (payment.processed === true) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  };
+
+  const canUserPlaceOrderRefactorTwo = (payment: any, product: any) => {
+    if (product.hasStock === false) {
+      return false;
+    } else if (payment.processed === true) {
+      return true;
+    }
+  };
+
+  const canUserPlaceOrderRefactorThree = (payment: any, product: any) => {
+    if (product.hasStock === true && payment.processed === true) {
+      return true;
+    }
+    return false;
+  };
+
+  const canUserPlaceOrderRefactorFour = (payment: any, product: any) => {
+    if (product.hasStock && payment.processed) {
+      return true;
+    }
+    return false;
+  };
+
+  const canUserPlaceOrderRefactorFive = (payment: any, product: any) => {
+    return product.hasStock && payment.processed;
+  };
+
+  const canUserPlaceOrderRefactorSix = (
+    { processed }: { processed: boolean },
+    { hasStock }: { hasStock: boolean }
+  ) => processed && hasStock;
+
+  return (
+    <div>
+      <h2>Inside my code is the examples!!!👋</h2>
+    </div>
+  );
+}
